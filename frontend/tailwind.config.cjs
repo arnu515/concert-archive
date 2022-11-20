1c441af40d8dc593c3c80f79cccaf493f598b227
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-11-21 04:01:24 +0530

    feat: add theme

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -5,7 +5,25 @@ const config = {
 		extend: {}
 	},
 
-	plugins: [require('daisyui')]
+	plugins: [require('daisyui')],
+
+	daisyui: {
+		themes: [
+			{
+				concert: {
+					primary: '#00A9A5',
+					secondary: '#3E5C76',
+					accent: '#cc3363',
+					neutral: '#191D24',
+					'base-100': '#242331',
+					info: '#3ABFF8',
+					success: '#85FF9E',
+					warning: '#F6AA1C',
+					error: '#AC3931'
+				}
+			}
+		]
+	}
 };
 
 module.exports = config;
