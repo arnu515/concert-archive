79baff3e07f3cd4e46524530f6bf686e729d2376
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-11-24 06:05:12 +0530

    feat: fetch user

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -1,7 +1,15 @@
+<script>
+  import { user } from "$lib/stores/token";
+</script>
+
 <nav class="bg-base-100 border-b border-base-200 text-white px-4 py-2 flex items-center justify-between">
   <a href="/" class="text-lg font-bold">Concert</a>
 
   <div class="flex items-center gap-4">
+    {#if !$user}
     <a href="/auth" class="btn btn-primary btn-sm px-4 rounded text-white">Sign In</a>
+    {:else}
+    <a href="/new" class="btn btn-primary btn-sm px-4 rounded text-white">Create stage</a>
+    {/if}
   </div>
 </nav>
--- frontend/src/lib/stores/token.ts
+++ frontend/src/lib/stores/token.ts
@@ -1,8 +1,10 @@
+import type { User } from '$lib/util/types';
 import { writable } from 'svelte/store';
 
 export const token = writable<string | null>(null);
 export const timeout = writable<NodeJS.Timeout | null>(null);
 export const tokenLoading = writable<boolean>(true);
+export const user = writable<User | null>(null);
 
 export async function refreshToken(f = fetch) {
 	const res = await f('/api/auth/refresh', { credentials: 'include' });
@@ -15,6 +17,19 @@ export async function refreshToken(f = fetch) {
 		token.set(json.access_token);
 	} else {
 		token.set(null);
+		tokenLoading.set(false);
+		return;
+	}
+
+	const res2 = await f('/api/auth/me', {
+		headers: { Authorization: 'Bearer ' + json.access_token }
+	});
+	const json2: { user: User } = await res2.json();
+	if (res.ok) {
+		user.set(json2.user);
+	} else {
+		token.set(null);
+		user.set(null);
 	}
 
 	tokenLoading.set(false);
