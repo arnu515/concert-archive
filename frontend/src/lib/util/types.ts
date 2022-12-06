967f1524659140e8e9eb8cfbd0cfb7ea51f8d77b
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-12-07 04:59:13 +0530

    feat: add stage type

--- frontend/src/lib/util/types.ts
+++ frontend/src/lib/util/types.ts
@@ -5,3 +5,12 @@ export interface User {
 	avatar_url: string;
 	created_at: string;
 }
+
+export interface Stage {
+	name: string;
+	private: boolean;
+	owner: User;
+	id: string;
+	owner_id: string;
+	color: string;
+}
