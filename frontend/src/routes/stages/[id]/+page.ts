6c67442709d369e675f6490797b94381d6e95715
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-12-08 04:56:23 +0530

    feat: wait for token to fetch

--- frontend/src/routes/stages/[id]/+page.ts
+++ frontend/src/routes/stages/[id]/+page.ts
@@ -1,10 +1,15 @@
+import { browser } from '$app/environment';
 import token from '$lib/stores/token';
 import { fetch } from '$lib/util/fetch';
 import { error } from '@sveltejs/kit';
 import { get } from 'svelte/store';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ params, fetch: f }) => {
+export const load: PageLoad = async ({ params, fetch: f, parent }) => {
+	// wait for +layout.ts to finish getting the access token
+	if (!browser) return;
+	await parent();
+
 	const id = params.id;
 	const res = await fetch(
 		`/api/stages/${id}`,
